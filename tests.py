f394110780d883b795e72aca7ef466501babbb16
fearsd <rifatfazlutdinov@gmail.com>
2021-07-15 15:22:47 +0500

    idea adding

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 """Tests."""
-
-from handlers import register_user
+import datetime
+from handlers import add_new_idea, register_user_or_find_existed
 
 
 def test_sum():
@@ -21,7 +21,7 @@ def test_register_user(test_db, user_data):
         test_db: Db session.
         user_data: User data.
     """
-    user = register_user(user_data=user_data, db=test_db)
+    user = register_user_or_find_existed(user_data=user_data, db=test_db)
     assert user.telegram_id == user_data['telegram_id']  # noqa: S101
 
 
@@ -33,9 +33,30 @@ def test_register_user_if_user_restarted_bot(test_db, user_data):
         test_db: Db session.
         user_data: User data.
     """
-    user_first_start = register_user(user_data=user_data, db=test_db)
+    user_first_start = register_user_or_find_existed(user_data=user_data, db=test_db)
 
-    user_second_start = register_user(user_data=user_data, db=test_db)
+    user_second_start = register_user_or_find_existed(user_data=user_data, db=test_db)
     assert user_first_start.telegram_id == user_data['telegram_id']  # noqa: S101, E501
     assert user_first_start.telegram_id == user_second_start.telegram_id  # noqa: S101, E501
     assert user_first_start.id == user_second_start.id  # noqa: S101
+
+
+def test_add_new_idea(test_db, user_data):
+    """
+    Tests adding new idea.
+
+    Parameters:
+        test_db: Db session.
+        user_data: User data.
+    """
+    user = register_user_or_find_existed(user_data=user_data, db=test_db)
+    idea_data = {
+        'user_id': user.id,
+        'body': 'text',
+        'date_created': datetime.datetime.now()
+    }
+    idea = add_new_idea(idea_data=idea_data, db=test_db)
+
+    assert idea.user_id == idea_data['user_id']  # noqa: S101
+    assert idea.body == idea_data['body']  # noqa: S101
+    assert idea.date_created == idea_data['date_created']  # noqa: S101
